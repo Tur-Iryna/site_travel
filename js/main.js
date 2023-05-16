7c2decf9c412a03f151ca2897b96f6b9d106be31
Tur-Iryna <115189368+Tur-Iryna@users.noreply.github.com>
2023-05-16 12:49:14 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
 
+const deadline = $('.discount__clock').attr('data-time');
+initializeClock('.discount__clock', deadline);
+
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+};
+
+function initializeClock(id, endtime) {
+  const clock = document.querySelector('.discount__clock');
+  const hoursSpan = clock.querySelector('.discount__hours');
+  const daysSpan = clock.querySelector('.discount__days');
+  const minutesSpan = clock.querySelector('.discount__minutes');
+  const secondsSpan = clock.querySelector('.discount__seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+};
+
+
 
 
 
@@ -29,52 +74,6 @@ window.onclick = function (event) {
     }
   }
 }
-
-
-
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
-
-function initializeClock(id, endtime) {
-  const clock = document.querySelector('.discount__clock');
-  const hoursSpan = clock.querySelector('.discount__hours');
-  const daysSpan = clock.querySelector('.discount__days');
-  const minutesSpan = clock.querySelector('.discount__minutes');
-  const secondsSpan = clock.querySelector('.discount__seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
-
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
-
-const deadline = $('.discount__clock').attr('data-time');
-initializeClock('discount__clock', deadline);
-
 
 
 $(function () {
